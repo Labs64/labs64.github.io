8f01ab284e53a14178e674b03bb0c4fa89adc792
Alexey Averikhin <alexey.averikhin@labs64.de>
2022-08-11 22:57:38 +0200

    Test cc

--- assets/vendor/cookieconsent/cookieconsent-init.js
+++ assets/vendor/cookieconsent/cookieconsent-init.js
@@ -1,25 +1,26 @@
-var LOREM_IPSUM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
-
 // obtain cookieconsent plugin
 var cc = initCookieConsent();
 
+var cookie = '🍪';
+
 // run plugin with config object
 cc.run({
-    current_lang: 'en',
-    autoclear_cookies: true,                    // default: false
-    cookie_name: 'cc_cookie_demo2',             // default: 'cc_cookie'
-    cookie_expiration: 365,                     // default: 182
+    current_lang : 'en',
+    autoclear_cookies : true,                   // default: false
+    cookie_name: 'l64_cc',                      // default: 'cc_cookie'
+    cookie_expiration : 365,                    // default: 182
     page_scripts: true,                         // default: false
     force_consent: true,                        // default: false
 
     // auto_language: null,                     // default: null; could also be 'browser' or 'document'
     // autorun: true,                           // default: true
     // delay: 0,                                // default: 0
+    // force_consent: false,
     // hide_from_bots: false,                   // default: false
     // remove_cookie_tables: false              // default: false
     // cookie_domain: location.hostname,        // default: current domain
-    // cookie_path: '/',                        // default: root
-    // cookie_same_site: 'Lax',
+    // cookie_path: "/",                        // default: root
+    // cookie_same_site: "Lax",
     // use_rfc_cookie: false,                   // default: false
     // revision: 0,                             // default: 0
 
@@ -30,8 +31,8 @@ cc.run({
             transition: 'slide'                 // zoom,slide
         },
         settings_modal: {
-            layout: 'bar',                      // box,bar
-            position: 'left',                   // right,left (available only if bar layout selected)
+            layout: 'box',                      // box,bar
+            // position: 'left',                // right,left (available only if bar layout selected)
             transition: 'slide'                 // zoom,slide
         }
     },
@@ -41,12 +42,11 @@ cc.run({
     },
 
     onAccept: function (cookie) {
-        console.log('onAccept fired!')
+        console.log('onAccept fired');
     },
 
     onChange: function (cookie, changed_preferences) {
-        console.log('onChange fired!');
-
+        console.log('onChange fired');
         // If analytics category is disabled => disable google analytics
         if (!cc.allowedCategory('analytics')) {
             typeof gtag === 'function' && gtag('consent', 'update', {
@@ -58,92 +58,78 @@ cc.run({
     languages: {
         'en': {
             consent_modal: {
-                title: 'Hello traveller, it\'s cookie time!',
-                description: 'Our website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <a href="https://www.labs64.com/legal/privacy-policy/" class="cc-link">Privacy policy</a>',
+                title: cookie + ' We use cookies! ',
+                description: 'Hi, this website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <button type="button" data-cc="c-settings" class="cc-link">Let me choose</button>',
                 primary_btn: {
                     text: 'Accept all',
-                    role: 'accept_all'      //'accept_selected' or 'accept_all'
+                    role: 'accept_all'              // 'accept_selected' or 'accept_all'
                 },
                 secondary_btn: {
-                    text: 'Preferences',
-                    role: 'settings'       //'settings' or 'accept_necessary'
-                },
-                revision_message: '<br><br> Dear user, terms and conditions have changed since the last time you visisted!'
+                    text: 'Reject all',
+                    role: 'accept_necessary'        // 'settings' or 'accept_necessary'
+                }
             },
             settings_modal: {
-                title: 'Cookie settings',
-                save_settings_btn: 'Save current selection',
+                save_settings_btn: 'Save settings',
                 accept_all_btn: 'Accept all',
                 reject_all_btn: 'Reject all',
                 close_btn_label: 'Close',
                 cookie_table_headers: [
                     {col1: 'Name'},
                     {col2: 'Domain'},
-                    {col3: 'Expiration'}
+                    {col3: 'Expiration'},
+                    {col4: 'Description'}
                 ],
                 blocks: [
                     {
-                        title: 'Cookie usage',
-                        description: LOREM_IPSUM + ' <a href="#" class="cc-link">Privacy Policy</a>.'
+                        title: 'Cookie usage 📢',
+                        description: 'We use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full <a href="https://www.labs64.com/legal/privacy-policy/" class="cc-link">privacy policy</a>.'
                     }, {
                         title: 'Strictly necessary cookies',
-                        description: LOREM_IPSUM + LOREM_IPSUM + "<br><br>" + LOREM_IPSUM + LOREM_IPSUM,
+                        description: 'These cookies are essential for the proper functioning of our website. Without these cookies, the website would not work properly',
                         toggle: {
                             value: 'necessary',
                             enabled: true,
-                            readonly: true  //cookie categories with readonly=true are all treated as "necessary cookies"
+                            readonly: true          // cookie categories with readonly=true are all treated as "necessary cookies"
                         }
                     }, {
-                        title: 'Analytics & Performance cookies',
-                        description: LOREM_IPSUM,
+                        title: 'Performance and Analytics cookies',
+                        description: 'These cookies allow the website to remember the choices you have made in the past',
                         toggle: {
-                            value: 'analytics',
+                            value: 'analytics',     // there are no default categories => you specify them
                             enabled: false,
                             readonly: false
                         },
                         cookie_table: [
                             {
                                 col1: '^_ga',
-                                col2: 'yourdomain.com',
-                                col3: 'description ...',
+                                col2: 'google.com',
+                                col3: '2 years',
+                                col4: 'description ...',
                                 is_regex: true
                             },
                             {
                                 col1: '_gid',
-                                col2: 'yourdomain.com',
-                                col3: 'description ...',
-                            },
-                            {
-                                col1: '_my_cookie',
-                                col2: 'yourdomain.com',
-                                col3: 'test cookie with custom path ...',
-                                path: '/demo'       // needed for autoclear cookies
+                                col2: 'google.com',
+                                col3: '1 day',
+                                col4: 'description ...',
                             }
                         ]
                     }, {
-                        title: 'Targeting & Advertising cookies',
-                        description: 'If this category is deselected, <b>the page will reload when preferences are saved</b>... <br><br>(demo example with reload option enabled, for scripts like microsoft clarity which will re-set cookies and send beacons even after the cookies have been cleared by the cookieconsent\'s autoclear function)',
+                        title: 'Advertisement and Targeting cookies',
+                        description: 'These cookies collect information about how you use the website, which pages you visited and which links you clicked on. All of the data is anonymized and cannot be used to identify you',
                         toggle: {
                             value: 'targeting',
                             enabled: false,
-                            readonly: false,
-                            reload: 'on_disable'            // New option in v2.4, check readme.md
-                        },
-                        cookie_table: [
-                            {
-                                col1: '^_cl',               // New option in v2.4: regex (microsoft clarity cookies)
-                                col2: 'yourdomain.com',
-                                col3: 'These cookies are set by microsoft clarity',
-                                // path: '/',               // New option in v2.4
-                                is_regex: true              // New option in v2.4
-                            }
-                        ]
+                            readonly: false
+                        }
                     }, {
                         title: 'More information',
-                        description: LOREM_IPSUM + ' <a class="cc-link" href="https://labs64.com/contact/">Contact me</a>.',
+                        description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://labs64.com/contact">contact me</a>.',
                     }
                 ]
             }
         }
     }
+
 });

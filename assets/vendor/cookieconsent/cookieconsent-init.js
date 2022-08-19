db0556220f4dad44e4145fd68a42727169c519af
Alexey Averikhin <alexey.averikhin@labs64.de>
2022-08-19 07:30:29 +0200

    Revisit cc

--- assets/vendor/cookieconsent/cookieconsent-init.js
+++ assets/vendor/cookieconsent/cookieconsent-init.js
@@ -58,7 +58,7 @@ cc.run({
         'en': {
             consent_modal: {
                 title: cookie + ' We use cookies!',
-                description: 'Hi, this website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <button type="button" data-cc="c-settings" class="cc-link">Let me choose</button>',
+                description: 'This website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <button type="button" data-cc="c-settings" class="cc-link">Let me choose</button>',
                 primary_btn: {
                     text: 'Accept all',
                     role: 'accept_all'              // 'accept_selected' or 'accept_all'
@@ -74,19 +74,13 @@ cc.run({
                 accept_all_btn: 'Accept all',
                 reject_all_btn: 'Reject all',
                 close_btn_label: 'Close',
-                cookie_table_headers: [
-                    {col1: 'Name'},
-                    {col2: 'Domain'},
-                    {col3: 'Expiration'},
-                    {col4: 'Description'}
-                ],
                 blocks: [
                     {
                         title: 'Cookie usage 📢',
                         description: 'We use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the <a href="https://www.labs64.com/legal/privacy-policy/" class="cc-link">privacy policy</a>.'
                     }, {
-                        title: 'Strictly necessary cookies',
-                        description: 'These cookies are essential for the proper functioning of our website. Without these cookies, the website would not work properly',
+                        title: 'Technically required cookies',
+                        description: 'These cookies are essential in order to enable you to move around the website and use its features, such as setting your privacy preferences, logging in or filling in forms. Without these cookies, services requested through usage of our website cannot be properly provided.',
                         toggle: {
                             value: 'necessary',
                             enabled: true,
@@ -94,32 +88,17 @@ cc.run({
                         }
                     }, {
                         title: 'Performance and Analytics cookies',
-                        description: 'These cookies allow the website to remember the choices you have made in the past',
+                        description: 'These cookies collect information about how visitors use a website, for instance which pages visitors go to most often, and how visitors move around the site. They help us to improve the user friendliness of a website and therefore enhance the user’s experience.',
                         toggle: {
-                            value: 'analytics',     // there are no default categories => you specify them
+                            value: 'analytics',    
                             enabled: false,
                             readonly: false
-                        },
-                        cookie_table: [
-                            {
-                                col1: '^_ga',
-                                col2: 'google.com',
-                                col3: '2 years',
-                                col4: 'description ...',
-                                is_regex: true
-                            },
-                            {
-                                col1: '_gid',
-                                col2: 'google.com',
-                                col3: '1 day',
-                                col4: 'description ...',
-                            }
-                        ]
+                        }
                     }, {
-                        title: 'Advertisement and Targeting cookies',
-                        description: 'These cookies collect information about how you use the website, which pages you visited and which links you clicked on. All of the data is anonymized and cannot be used to identify you',
+                        title: 'Functionality and Usability cookies',
+                        description: 'These cookies allow the website to remember choices you make or information you enter (such as your username, language or the region you are in) and provide enhanced, more personal features. They are also used to enable enhanced website functions.',
                         toggle: {
-                            value: 'targeting',
+                            value: 'usability',    
                             enabled: false,
                             readonly: false
                         }
